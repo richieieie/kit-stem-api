84645163498756172c322156e476f05260da645b
richieieie <trungnguyen0803forwork@gmail.com>
2024-09-12 22:09:08 +0700

    restart

--- obj/Debug/net8.0/kit-stem-api.AssemblyInfo.cs
+++ obj/Debug/net8.0/kit-stem-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("kit-stem-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f35daaffb7b76c5cb88e90b2cc3c6848f68245e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+911d3b6f2e7a132d6640f80734ac066a056bebc6")]
 [assembly: System.Reflection.AssemblyProductAttribute("kit-stem-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("kit-stem-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
