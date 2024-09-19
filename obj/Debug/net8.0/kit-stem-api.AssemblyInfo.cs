c8b409663d7c94ae21df00fa0cc8a742c6d45f8d
khanhhoang209 <lehoangkhanh209@gmail.com>
2024-09-19 17:21:32 +0700

    Fix conflict

--- obj/Debug/net8.0/kit-stem-api.AssemblyInfo.cs
+++ obj/Debug/net8.0/kit-stem-api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("kit-stem-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+167b83b2a26c10cb61344f8cb2c8a07522c1c981")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+118bfe1b97d8252191fb533a62b817d4712e93c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("kit-stem-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("kit-stem-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
