792a70a9c2d592ceb2a201e0a8c00d54ef73c5dc
Trinh Duc Hung <hungductrinh2004@gmail.com>
2024-10-01 21:17:55 +0700

    Revert "Squashed commit of the following:"
    This reverts commit babbe801427861e7ee728bdd28d9ed5bee89ab45.

--- Models/DTO/Response/KitResponseDTO.cs
+++ Models/DTO/Response/KitResponseDTO.cs
@@ -1,19 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using kit_stem_api.Models.Domain;
+﻿using kit_stem_api.Models.Domain;
+using System.ComponentModel.DataAnnotations;
 
 namespace kit_stem_api.Models.DTO.Response
 {
-    public class KitInPackageResponseDTO
+    public class KitResponseDTO
     {
         public int Id { get; set; }
+
+        public int CategoryId { get; set; }
+
         public string Name { get; set; } = null!;
+
         public string Brief { get; set; } = null!;
+
         public string Description { get; set; } = null!;
+
         public int PurchaseCost { get; set; }
+
         public bool Status { get; set; }
-        public virtual KitsCategory Category { get; set; } = null!;
+
+        public virtual KitsCategory? KitsCategory { get; set; }
     }
 }
--- Repositories/UnitOfWork.cs
+++ Repositories/UnitOfWork.cs
@@ -28,7 +28,6 @@ namespace kit_stem_api.Repositories
                         PackageLabRepository = new PackageLabRepository(_dbContext);
                         KitComponentRepository = new KitComponentRepository(_dbContext);
                         KitRepository = new KitRepository(_dbContext);
-                        PackageLabRepository = new PackageLabRepository(_dbContext);
                 }
         }
 }
