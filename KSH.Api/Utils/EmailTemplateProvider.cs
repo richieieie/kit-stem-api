91087c127d39aac75b7717073637347a4ef0cb1b
richieieie <trungnguyen0803forwork@gmail.com>
2024-10-11 20:50:29 +0700

    Add API for sending verification email

---
+++ KSH.Api/Utils/EmailTemplateProvider.cs
@@ -0,0 +1,30 @@
+using KST.Api.Utils.Interfaces;
+
+namespace KST.Api.Utils
+{
+    public class EmailTemplateProvider : IEmailTemplateProvider
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public EmailTemplateProvider(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public string GetRegisterTemplate(string userName, string shopName, string verifyUrl)
+        {
+            string body = string.Empty;
+            string path = Path.Combine(_webHostEnvironment.ContentRootPath, "Assets", "Templates", "Register.html");
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                body = reader.ReadToEnd();
+            }
+
+            body = body.Replace("[UserName]", userName);
+            body = body.Replace("[ShopName]", shopName);
+            body = body.Replace("[VerifyUrl]", verifyUrl);
+
+            return body;
+        }
+    }
+}
