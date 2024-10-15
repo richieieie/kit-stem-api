f644f83e71fce18bca169f60f963db6b8ae3f10c
Trinh Duc Hung <hungductrinh2004@gmail.com>
2024-10-15 15:54:57 +0700

    update issues Kit Api #92

--- KSH.Api/Models/DTO/Response/PackageInKitDTO.cs
+++ KSH.Api/Models/DTO/Response/PackageInKitDTO.cs
@@ -9,7 +9,6 @@ namespace KSH.Api.Models.DTO.Response
         public int Price { get; set; }
         public bool Status { get; set; }
         public virtual Level? Level { get; set; }
-        public virtual KitResponseDTO? Kit { get; set; }
         public virtual ICollection<LabInPackageResponseDTO>? PackageLabs { get; set; }
     }
 }
