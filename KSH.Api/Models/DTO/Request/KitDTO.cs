c21aea0ab6cc32174b0b16111bd09f80db4f5a56
Trinh Duc Hung <hungductrinh2004@gmail.com>
2024-10-25 22:07:32 +0700

    update
    fix merge Main

--- KSH.Api/Models/DTO/Request/KitDTO.cs
+++ KSH.Api/Models/DTO/Request/KitDTO.cs
@@ -4,13 +4,9 @@ namespace KSH.Api.Models.DTO.Request
 {
     public class KitDTO
     {
-        [Required(ErrorMessage = "Vui lòng nhập ID")]
+        
         public int Id { get; set; }
-        [Required(ErrorMessage = "Vui lòng nhập giá mua")]
-        [Range(0, int.MaxValue, ErrorMessage = "Giá mua phải lớn hơn hoặc bằng 0")]
-        public int PurchaseCost { get; set; }
-        [Required(ErrorMessage = "Vui lòng nhập số lượng")]
-        [Range(0, int.MaxValue, ErrorMessage = "Số lượng phải lớn hơn hoặc bằng 0")]
+        public long PurchaseCost { get; set; }
         public int Quantity { get; set; }
     }
 }
