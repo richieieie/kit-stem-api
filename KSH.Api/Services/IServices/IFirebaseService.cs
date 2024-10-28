fefcb36e6858899288b41d5c19cb61ec2f1c1c9c
richieieie <trungnguyen0803forwork@gmail.com>
2024-10-28 09:12:39 +0700

    Add Get Lab File Url

--- KSH.Api/Services/IServices/IFirebaseService.cs
+++ KSH.Api/Services/IServices/IFirebaseService.cs
@@ -5,5 +5,6 @@ namespace KSH.Api.Services.IServices
         Task<ServiceResponse> UploadFileAsync(string bucket, string folder, string fileName, IFormFile file);
         Task<ServiceResponse> UploadFilesAsync(string bucket, string folder, Dictionary<string, IFormFile>? nameFiles);
         Task<ServiceResponse> DownloadFileAsync(string bucket, string pathToFile);
+        ServiceResponse GetSignedUrlAsync(string bucket, string filePath, int expirationInMinutes = 15);
     }
 }
