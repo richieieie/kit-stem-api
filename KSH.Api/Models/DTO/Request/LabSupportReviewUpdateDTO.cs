cae90c11587c5f00eb75a3b192eb702e322e44e5
Trinh Duc Hung <hungductrinh2004@gmail.com>
2024-11-13 17:45:13 +0700

    Update LabSupportReviewUpdateDTO.cs
    fix back

--- KSH.Api/Models/DTO/Request/LabSupportReviewUpdateDTO.cs
+++ KSH.Api/Models/DTO/Request/LabSupportReviewUpdateDTO.cs
@@ -8,7 +8,6 @@ namespace KST.Api.Models.DTO.Request
         public Guid Id { get; set; }
         [Range(1, int.MaxValue, ErrorMessage = "Xếp hạng phải lớn hơn hoặc bằng 1")]
         public int Rating { get; set; }
-        [Required(ErrorMessage = "Vui lòng thêm đánh giá")]
-        public string FeedBack { get; set; } = "không có đánh giá";
+        public string? FeedBack { get; set; }
     }
 }
