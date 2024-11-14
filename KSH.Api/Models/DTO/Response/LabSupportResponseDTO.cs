f42ed3866526fb1d15cf901b4a210316c54f4ab8
Trinh Duc Hung <hungductrinh2004@gmail.com>
2024-11-14 08:43:27 +0700

    Update
    Add Customer Phone number get from Order

--- KSH.Api/Models/DTO/Response/LabSupportResponseDTO.cs
+++ KSH.Api/Models/DTO/Response/LabSupportResponseDTO.cs
@@ -13,6 +13,7 @@ namespace KST.Api.Models.DTO.Response
         public string? FeedBack { get; set; }
         public bool IsFinished { get; set; }
         public DateTimeOffset CreatedAt { get; set; }
+        public string? CustomerPhoneNumber { get; set; }
         public virtual UserStaffInLabSupportDTO? Staff { get; set; }
         public virtual UserInLabSupportDTO? User { get; set; }
         public virtual LabInLabSupportResponseDTO? Lab { get; set; }
